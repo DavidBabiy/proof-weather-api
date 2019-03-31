534c28f34e810f227edefa777681f9c02b9d98d5
DavidBabiy <david.babiy1995@gmail.com>
2019-03-31 20:12:15 +0300

    add npm

---
+++ test/Data.spec.ts
@@ -0,0 +1,17 @@
+import * as properties from "./properties";
+
+let WEB_URL = properties.WEB_URL;
+
+describe('Weather API', async () => {
+
+    describe('Current weather data', async () => {
+
+        it('should get weather data by city id', function () {
+
+
+
+        });
+
+    });
+
+});
